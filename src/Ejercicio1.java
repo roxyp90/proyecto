e04db88c4711c0823a2e0ba89fa3bace25acde0e
Roxanne Perez <salomeperezd90@gmail.com>
2025-02-28 13:12:43 -0500

    Se arregla SRC, y se sube el ejercicio 1 más la estructura para ejercicio Mediana

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Ejercicio1 {
     public static void main(String[] args) {
         // Crear un objeto Scanner para leer la entrada del usuario
         Scanner input = new Scanner(System.in);
---
+++ src/Mediana.java
@@ -0,0 +1,5 @@
+public class Mediana {
+    public static void main(String[] args) {
+        
+    }
+}
