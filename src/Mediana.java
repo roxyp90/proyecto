99aa2dd04f6a12e9ecf2959192d030d26d6d9f01
Roxanne Perez <salomeperezd90@gmail.com>
2025-02-28 13:45:58 -0500

    mediana

--- src/Mediana.java
+++ src/Mediana.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Mediana {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Introduce el tercer número: ");
+        double num3 = scanner.nextDouble();
+
+        double media = (num1 + num2 + num3) / 3;
+
+        // Resultado
+        System.out.printf("La media de los tres números es:" + media);
+        scanner.close();
     }
 }
