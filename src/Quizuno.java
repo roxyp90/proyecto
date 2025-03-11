5277b5066f5d7e1909b0d8b3d4d38a7baaa81dc6
roxanne <salomeperezd90@gmail.com>
2025-03-11 07:47:33 -0500

    ejercicios

---
+++ src/Quizuno.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Quizuno
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero positivo: ");
+        int num = input.nextInt();
+
+        if (num <= 0) {
+            System.out.println("Error: Debe ingresar un número entero positivo.");
+            input.close();
+            return;
+        }
+
+        for (int i = num; i >= 1; i--) {
+            for (int espacio = 1; espacio < i; espacio++) {
+                System.out.print("  ");
+            }
+
+            for (int j = i; j <= num; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = num - 1; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
+
+
--- src/TablaMultiplicar.java
+++ src/TablaMultiplicar.java
@@ -9,5 +9,6 @@ public class TablaMultiplicar {
         for (int i = 1; i <= 10; i++) {
             System.out.println(numero + " x " + i + " = " + (numero * i));
         }
+        scanner.close();
     }
 }
