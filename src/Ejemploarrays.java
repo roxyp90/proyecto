44205c637e8cfb9fddb0a3a4341f05dd07a0d2de
roxanne <salomperezd90@gmail.com>
2025-03-25 07:44:27 -0500

    ejercicios

---
+++ src/Ejemploarrays.java
@@ -0,0 +1,21 @@
+public class Ejemploarrays {
+    public static void main(String[] args) {
+        // Declarar e inicializar el array de valores
+        double[] precios = {12.5, 30.0, 7.8, 45.2, 22.9};
+
+        // Imprimir los valores usando un bucle for
+        System.out.println("Valores en el array:");
+        for (int indice = 0; indice < precios.length; indice++) {
+            System.out.println("Valor en la posición " + indice + ": " + precios[indice]);
+        }
+
+        // Modificar un valor en el array
+        precios[2] = 15.5;
+
+        // Imprimir el array modificado
+        System.out.println("\nArray de valores modificado:");
+        for (int posicion = 0; posicion < precios.length; posicion++) {
+            System.out.println("Valor en la posición " + posicion + ": " + precios[posicion]);
+        }
+    }
+}
---
+++ src/Promedioarray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Promedioarray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      
+        int[] numeros = new int[5];
+        int suma = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingresa el número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+            suma += numeros[i];
+        }
+
+        
+        double promedio = (double) suma / numeros.length;
+
+        
+        System.out.println("El promedio de los números ingresados es: " + promedio);
+
+        scanner.close();
+    }
+}
